d954532585850a4ae6fca280976e589afd739df9
AsadMoinuddin152 <160920733152@lords.ac.in>
2024-01-30 22:35:39 +0530

    Perform operations on lists: capitalize pet names, zip and sort lists, filter scores over 50%, and calculate the total using reduce.

--- 26-1-24 (Day 5)/1.py
+++ 26-1-24 (Day 5)/1.py
@@ -1,29 +1,40 @@
 from functools import reduce
 
-#1 Capitalize all of the pet names and print the list
+# Task 1: Capitalize all pet names in the list
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
+
+# Function to capitalize a string
 def Capitalize(i):
     return i.upper()
 
-print(list(map(Capitalize, my_pets)))
+# Apply the Capitalize function to each element in the list using map
+capitalized_pets = list(map(Capitalize, my_pets))
+print(capitalized_pets)
 
-
-
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+# Task 2: Zip two lists into a list of tuples, sorting the numbers from lowest to highest
 my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
+my_numbers = [5, 4, 3, 2, 1]
+
+# Create a list of tuples by zipping my_strings and sorted my_numbers
 my_zip = list(zip(my_strings, sorted(my_numbers)))
 print(my_zip)
 
-#3 Filter the scores that pass over 50%
+# Task 3: Filter scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
+
+# Function to filter scores over 50%
 def remove_under_50(i):
     return i > 50
-print(list(filter(remove_under_50, scores)))
 
+# Use filter to select scores over 50%
+filtered_scores = list(filter(remove_under_50, scores))
+print(filtered_scores)
 
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+# Task 4: Combine all numbers in two lists using reduce and calculate the total
+# Function to accumulate the sum of two numbers
 def accumulator(acc, item):
     return acc + item
 
-print(reduce(accumulator, (my_numbers + scores)))
+# Use reduce to accumulate the sum of my_numbers and scores
+total_sum = reduce(accumulator, (my_numbers + scores))
+print(total_sum)
