93b92b328122e2fb161df1dc6fbe4b3e5ebd7d15
AsadMoinuddin152 <160920733152@lords.ac.in>
2024-01-30 22:34:36 +0530

    Implement bubble sort for list sorting and demonstrate abstract classes in Python.

--- 25-1-24 (Day 4)/1.py
+++ 25-1-24 (Day 4)/1.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+# Abstract base class 'Animal' with abstract methods 'sound' and 'eat'
 class Animal(ABC):
     @abstractmethod
     def sound(self):
@@ -8,13 +9,16 @@ class Animal(ABC):
     @abstractmethod
     def eat(self):
         print('Animal eating')
+
+# Concrete class 'pet_animals' inheriting from 'Animal'
 class pet_animals(Animal):
     def sound(self):
         print("pet animals sound")
 
     def eat(self):
         print("pet animals eat")
-        
+
+# Abstract base class 'Loan' with abstract methods 'get_interest_rate' and 'calculate_interest'
 class Loan(ABC):
     @abstractmethod
     def get_interest_rate(self):
@@ -23,13 +27,18 @@ class Loan(ABC):
     @abstractmethod
     def calculate_interest(self):
         pass
+
+# Concrete class 'general_loan' inheriting from 'Loan'
 class general_loan(Loan):
     def get_interest_rate(self):
         print("general loan interest rate")
 
     def calculate_interest(self):
         print("general loan calculate interest")
+
+# Instantiate objects of concrete classes
 pet_obj = pet_animals()
 loan_obj = general_loan()
 
+# Invoke the 'eat' method of 'pet_animals' and print the result
 print(pet_obj.eat())
--- 25-1-24 (Day 4)/2.py
+++ 25-1-24 (Day 4)/2.py
@@ -1,9 +1,19 @@
 def bubble_sort(num):
+    """
+    Perform bubble sort on a list of numbers.
+
+    Parameters:
+    - num (list): List of numbers to be sorted.
+
+    Returns:
+    - list: Sorted list in ascending order.
+    """
     for i in range(len(num)):
         for j in range(len(num)-1):
             if num[j] > num[j+1]:
                 num[j], num[j+1] = num[j+1], num[j]
     return num
 
+# Example usage
 num = [5, 2, 1, 3, 4]
 print(bubble_sort(num))
